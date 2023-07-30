47d384ae42b23fba8fb9e6e1202f8cd47fb1a030
hdswami <haridass_279@yahoo.com>
2023-07-30 23:01:12 +0530

    deploy web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "689687accfc0518557e98e19c1e93ab2",
-"/": "689687accfc0518557e98e19c1e93ab2",
-"main.dart.js": "5ae3025aeed09c2ea5ffd447cfae5f56",
+"index.html": "e24364c22637597e6130834ab59c176b",
+"/": "e24364c22637597e6130834ab59c176b",
+"main.dart.js": "6cce4b4e2de384a24bdaa323298bdd82",
 "manifest.json": "8ddfbd491db2a6d2934baf9d33628900",
 "version.json": "b7e0e36301dfefc95d5fa5686684e6f8"
 };
